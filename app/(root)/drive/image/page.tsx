99a929679911efe4abe583fca11b14e1a9987ec0
Ashutosh <66782335+Ashutosh-pixel@users.noreply.github.com>
2025-07-13 16:33:18 +0530

    feat: basic upload bar created

--- app/(root)/drive/image/page.tsx
+++ app/(root)/drive/image/page.tsx
@@ -1,10 +1,10 @@
 'use client'
 import Image from '@/components/sidebar/Image'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import FileUpload from '@/components/fileupload/FileUpload'
 
 const page = () => {
-  let userID = "681cbca24c31bfa9b698a961"
+  const userID = "681cbca24c31bfa9b698a961"
   
   return (
     <div>
--- components/fileupload/FileUpload.tsx
+++ components/fileupload/FileUpload.tsx
@@ -1,30 +1,53 @@
 'use client'
-import {useState} from "react";
+import React from "react";
+import { useState } from "react";
 import axios from "axios";
+import { CircularProgressWithLabel } from "./CircularProgressWithLabel";
 
 
 const FileUpload = () => {
 
     const [file, setFile] = useState<File | null>(null);
-    const [userID, setUserID] = useState<string>("681cbca24c31bfa9b698a961");
+    const [progress, setProgress] = useState<number>(0);
+    const [uploading, setUploading] = useState<boolean>(false);
+    const [userID] = useState<string>("681cbca24c31bfa9b698a961");
 
     async function upload() {
-        if(!file) return;
+        if (!file) return;
         const formData = new FormData();
         formData.append('file', file);
         formData.append('userID', userID);
         console.log('formData', formData.get("userID"))
-        const response = await axios.post('http://localhost:3001/user/uploadfile', formData)
-        alert(response.data.message || response.data.error)
+        try {
+            setUploading(true);
+            const response = await axios.post('http://localhost:3001/user/uploadfile', formData, {
+                onUploadProgress: (progressEvent) => {
+                    const { loaded, total } = progressEvent;
+                    if (typeof total === "number" && total > 0) {
+                        const percentCompleted = Math.floor((loaded * 100) / total);
+                        console.log('loaded & total', loaded, total);
+                        setProgress(percentCompleted);
+                    }
+                },
+            })
+            alert(response.data.message || response.data.error)
+        } catch (error) {
+            console.log('error while uploading', error);
+        }
+        finally {
+            setUploading(false);
+        }
     }
 
     return (
         <div>
             <input type="file" onChange={(e) => {
-                if(e.target.files?.[0]){
-                    setFile(e.target.files[0]);}
-            }}/>
+                if (e.target.files?.[0]) {
+                    setFile(e.target.files[0]);
+                }
+            }} />
             <button onClick={upload}>Upload</button>
+            {uploading ? <CircularProgressWithLabel value={progress} /> : null}
         </div>
     )
 }
